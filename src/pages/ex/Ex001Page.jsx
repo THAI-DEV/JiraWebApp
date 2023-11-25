53b9f00e66acbf888d7acc96b51e4a1cd0c09278
Ruangdech  Kurunat <ruangdech@gmail.com>
2023-11-25 21:53:52 +0700

    add code : list

--- src/pages/ex/Ex001Page.jsx
+++ src/pages/ex/Ex001Page.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import CounterSignal from '../../components/counter/CounterSignal';
 
 function Ex001Page() {
+  const initData = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
+
+  // const list = useMemo(() => {
+  //   return initData;
+  // }, []);
+
+  const [list, setList] = useState(initData);
+
+  function deleteHandler(i) {
+    setList([...list.slice(0, i), ...list.slice(i + 1)]);
+  }
+
   return (
     <>
       <h3>Ex01</h3>
       {/* <CounterSignal /> */}
+      {list.map((item, index) => (
+        <div key={index}>
+          {item.name} <button onClick={() => deleteHandler(index)}>Delete</button>
+        </div>
+      ))}
     </>
   );
 }
