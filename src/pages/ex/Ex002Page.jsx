c97e1c760c32485eb3b4263150be5a7f79007464
Ruangdech  Kurunat <ruangdech@gmail.com>
2023-12-16 17:37:51 +0700

    update

--- src/pages/ex/Ex002Page.jsx
+++ src/pages/ex/Ex002Page.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { projectAll } from './../../service/service.js';
 
 function Ex002Page() {
+  async function clickHandler() {
+    await projectAll()
+      .then(function (result) {
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <>
       <h3>Ex02</h3>
+      <button onClick={clickHandler}>Exe</button>
     </>
   );
 }
