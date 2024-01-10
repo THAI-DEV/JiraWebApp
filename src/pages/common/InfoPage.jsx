e241d4a1f4f9ff654823bda5be87fec233a059d4
Ruangdech  Kurunat <ruangdech@gmail.com>
2024-01-10 12:09:37 +0700

    update info page

--- src/pages/common/InfoPage.jsx
+++ src/pages/common/InfoPage.jsx
@@ -9,7 +9,7 @@ function InfoPage() {
     <>
       {/* <h3>Home</h3> */}
       <Card title="Jira Info">
-        <h4 className="m-5">Rest Base Url : {REST_BASE_URL}</h4>
+        <h4 className="m-5">Jira Rest Service : {REST_BASE_URL}</h4>
         <hr />
         <h3 className="m-5">Last Build : {formatLocalStr(BUILD_DATE)}</h3>
         <h3 className="m-5">By : {BUILD_BY}</h3>
