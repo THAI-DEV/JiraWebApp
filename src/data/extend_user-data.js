edd3ccfc6d886fdad83f3cd78c2f7f0bbde78fff
Ruangdech  Kurunat <ruangdech@gmail.com>
2024-02-09 20:32:43 +0700

    ีupdate extendUserInfoData

--- src/data/extend_user-data.js
+++ src/data/extend_user-data.js
@@ -3,4 +3,12 @@ export const extendUserInfoData = [
     name: 'อั๋น',
     code: '712020:e0c1f4d1-f728-4fa1-aec0-5d3b4ecff8ba',
   },
+  {
+    name: 'น้ำ',
+    code: '712020:82de8efa-cb67-440c-ad79-c4b4d7bfc037',
+  },
+  {
+    name: 'เกมส์',
+    code: '712020:3ec50792-7989-43fc-a688-1e324241fc19',
+  },
 ];
