f08a7445ab5f8be515e28969a0321a2604388821
Ruangdech  Kurunat <ruangdech@gmail.com>
2024-11-11 23:39:37 +0700

    remove unused user data and update display names in user info

--- src/data/extend_user-data.js
+++ src/data/extend_user-data.js
@@ -3,28 +3,4 @@ export const extendUserInfoData = [
     name: 'อั๋น',
     code: '712020:e0c1f4d1-f728-4fa1-aec0-5d3b4ecff8ba',
   },
-  {
-    name: 'น้ำ',
-    code: '712020:82de8efa-cb67-440c-ad79-c4b4d7bfc037',
-  },
-  {
-    name: 'เกมส์',
-    code: '712020:3ec50792-7989-43fc-a688-1e324241fc19',
-  },
-  {
-    name: 'นีน',
-    code: '712020:af4fd845-587d-474a-b24a-45ca7878cdab',
-  },
-  {
-    name: 'เจน CO',
-    code: '634e19f8b0b6ef035646e523',
-  },
-  {
-    name: 'นิด',
-    code: '712020:7a05cda4-189b-4434-a819-5c0d9ba5215a',
-  },
-  {
-    name: 'Vannasith PHETPASEUTH',
-    code: '5f98c7a3c8247300701450ba',
-  },
 ];
